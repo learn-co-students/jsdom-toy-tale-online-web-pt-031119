e88deb1c6dadadf7d331d171ed673a330cd0b094
thistonyb <this.tonybaker@gmail.com>
2019-10-04 14:41:44 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+const addBtn = document.querySelector("#new-toy-btn");
+const toyForm = document.querySelector(".container");
+let addToy = false;
 
 // YOUR CODE HERE
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", () => {
   // hide & seek with the form
-  addToy = !addToy
+  addToy = !addToy;
   if (addToy) {
-    toyForm.style.display = 'block'
+    toyForm.style.display = "block";
   } else {
-    toyForm.style.display = 'none'
+    toyForm.style.display = "none";
   }
-})
-
+});
 
 // OR HERE!
+document.addEventListener("DOMContentLoaded", function() {
+  fetchToys();
+});
+
+function fetchToys() {
+  const toyUrl = "http://localhost:4000/toys";
+  fetch(toyUrl)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      renderToys(json);
+    });
+}
+
+function renderToys(json) {
+  const toyContainer = document.getElementById("toy-collection");
+  json.forEach(toy => {
+    const elem = document.createElement("div");
+  });
+}
