be64386331d00c94fea4324d1c9cf0ff469ae02e
Ryan <kerneltux0@gmail.com>
2019-10-09 10:36:07 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
+const toysURL = 'http://localhost:3000/toys';
+const toyList = document.querySelector('#toy-collection');
+
+// const toyDiv = document.createElement('div');
+// const toyTitle = document.createElement('h2');
+// const toyLikes = document.createElement('p');
+// const toyLikeBtn = document.createElement('button');
+// const toyImage = document.createElement('img');
+
+const toyName = document.getElementById('toy-name').value;
+const toyUrl = document.getElementById('toy-url').value;
 
 // YOUR CODE HERE
 
@@ -14,5 +25,43 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+// fetch
+function submitData(toyName,toyUrl){
+  const toyDiv = document.createElement('div');
+  const toyTitle = document.createElement('h2');
+  const toyLikes = document.createElement('p');
+  const toyLikeBtn = document.createElement('button');
+  const toyImage = document.createElement('img');
 
-// OR HERE!
+  const formData={
+    name: toyName,
+    image: toyUrl
+  };
+
+  const config={
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  };
+
+  fetch(toysURL,config)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    console.log(json);
+    toyList.appendChild(toyDiv);
+    toyDiv.setAttribute('class','card');
+    toyDiv.appendChild(toyTitle);
+    toyTitle.innerText = `${json.name}`;
+    toyImage.innerHTML = `src=${json.image}`;
+    toyImage.setAttribute('class','toy-avatar');
+    toyDiv.appendChild(toyImage);
+    toyDiv.appendChild(toyLikes);
+    toyLikeBtn.setAttribute('class','like-btn');
+    toyDiv.appendChild(toyLikeBtn);
+  });
+};
