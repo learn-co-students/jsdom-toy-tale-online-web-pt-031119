bf18c225283112e3209dcd865c5dea9de25120a9
iamtash <natashachernis@gmail.com>
2019-10-12 20:24:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const createNewToyBtn = toyForm.querySelector('input.submit')
 let addToy = false
+
+const toyCollectionDiv = document.querySelector('div#toy-collection')
+
+const url = 'http://localhost:3000/toys'
 
 // YOUR CODE HERE
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener('click', (e) => {
   // hide & seek with the form
   addToy = !addToy
   if (addToy) {
@@ -14,5 +19,24 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+fetchToyData(url)
 
-// OR HERE!
+const configObj = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+}
+
+createNewToyBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const toyFormData = {}
+  toyFormData.name = e.currentTarget.parentElement[0].value
+  toyFormData.image = e.currentTarget.parentElement[1].value
+  toyFormData.likes = 0
+  
+  configObj.body = JSON.stringify(toyFormData)
+  
+  fetchNewToy(url, configObj)
+})
---
+++ src/toytaleFunctions.js
@@ -0,0 +1,53 @@
+const fetchToyData = (url) => {
+    fetch(url)
+    .then(function(resp) {
+      return resp.json();
+    })
+      .then(function(objOfToys) {
+        renderToyCards(objOfToys)
+      })
+  }
+  
+  const generateToyCard = (toy) => {
+    let cardDiv = document.createElement('div')
+    cardDiv.className = 'card'
+  
+    let cardH2 = document.createElement('h2')
+    cardH2.innerText = toy.name 
+    cardDiv.appendChild(cardH2)
+  
+    let cardImg = document.createElement('img')
+    
+    cardImg.src = toy.image
+    cardDiv.appendChild(cardImg)
+  
+    let cardP = document.createElement('p')
+    cardP.innerText = toy.likes
+    cardDiv.appendChild(cardP)
+  
+    let cardButton = document.createElement('button')
+    cardButton.className = 'like-btn'
+    cardButton.innerText = 'Like <3'
+    cardDiv.appendChild(cardButton)
+  
+    return cardDiv
+  }
+  
+  const renderToyCards = (toys) => {
+    toys.forEach(function(toy) {
+      toyCollectionDiv.appendChild(generateToyCard(toy))
+    })
+  }
+
+const fetchNewToy = (url, configObj) => {
+    fetch(url, configObj)
+        .then(function(resp) {
+        return resp.json()
+        })
+            .then(function(toyObj) {
+                toyCollectionDiv.appendChild(generateToyCard(toyObj))
+            })
+                .catch(function(error) {
+                console.log(error.message)
+                })
+}
