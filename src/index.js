9ae2a8de87114a91aa2d11234852cf4efd2a49ce
Alex Merecka <merecka@gmail.com>
2019-10-16 15:09:01 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const toy_form_fields = document.querySelector('.add-toy-form')
 const toy_collect = document.querySelector('#toy-collection')
 
 
-function submitData(name, image) {
+function submitData(name, image, likes=0) {
 	let formData = {
 	  	name: `${name}`,
 	 	image: `${image}`,
-	 	likes: 0
+	 	likes: `${likes}`
 	};
 	let configObj = {
 	  method: "POST",
@@ -19,6 +19,7 @@ function submitData(name, image) {
 	  },
 	  body: JSON.stringify(formData)
 	};
+	
 	fetch("http://localhost:3000/toys", configObj)
 	 .then(function(response) {
 		return response.json();
@@ -35,46 +36,56 @@ function submitData(name, image) {
 
 function createToys(object) {
 	  	for (const toy of object) {
-	  		let toy_div = document.createElement('div.card')
-		  		toy_collect.appendChild(toy_div)
-		  		let h2 = document.createElement('h2')
-		  		h2.innerHTML = toy.name
-		  		toy_div.appendChild(h2)
-		  		let img = document.createElement('img')
-		  		img.classList.add("toy-avatar")
-		  		img.src = toy.image
-		  		toy_div.appendChild(img)
-		  		let p = document.createElement('p')
-		  		p.innerHTML = toy.likes
-		  		toy_div.appendChild(p)
-		  		let button = document.createElement('button')
-		  		button.classList.add("like-btn")
-		  		button.innerHTML = "Like"
-		  		toy_div.appendChild(button)
+	  		createOneToy(toy)
 	  	}	
 }
 
 function createOneToy(object) {
-	  		let toy_div = document.createElement('div.card')
-		  		toy_collect.appendChild(toy_div)
-		  		let h2 = document.createElement('h2')
-		  		h2.innerHTML = object.name
-		  		toy_div.appendChild(h2)
-		  		let img = document.createElement('img')
-		  		img.classList.add("toy-avatar")
-		  		img.src = object.image
-		  		toy_div.appendChild(img)
-		  		let p = document.createElement('p')
-		  		p.innerHTML = object.likes
-		  		toy_div.appendChild(p)
-		  		let button = document.createElement('button')
-		  		button.classList.add("like-btn")
-		  		button.innerHTML = "Like"
-		  		toy_div.appendChild(button)
-}	
+	  	let toy_div = document.createElement('div.card')
+			toy_collect.appendChild(toy_div)
+	  		let h2 = document.createElement('h2')
+	  		h2.innerHTML = object.name
+			toy_div.appendChild(h2)
+	  		let img = document.createElement('img')
+		  	img.classList.add("toy-avatar")
+		  	img.src = object.image
+		  	toy_div.appendChild(img)
+		  	let p = document.createElement('p')
+		  	p.innerHTML = object.likes
+		  	toy_div.appendChild(p)
+		  	let button = document.createElement('button')
+		  	button.classList.add("like-btn")
+		  	button.innerHTML = "Like"
+		  	toy_div.appendChild(button)
+		  	button.setAttribute('id', object.id)
+		  	button.addEventListener('click', () => {
+				let toy_button = event.target
+				let id = toy_button.id
+				let likes = toy_button.parentElement.querySelector('p')
+				let likes_int = parseInt(likes.innerText)
+				likes_int++
+				likes.innerHTML = likes_int
+				fetch(`http://localhost:3000/toys/${id}`, {
+					method: 'PATCH',
+					headers: 
+					{
+					  "Content-Type": "application/json",
+					  "Accept": "application/json"
+					},	 
+					body: JSON.stringify(
+					{
+					  "likes": likes_int
+					})
+				})
+				.catch(function(error) {
+					console.log(error)
+				    alert("There was an error updating the likes!")
+				 }) 
+				})
+}
 
 document.addEventListener('DOMContentLoaded', function() {   
-  fetch("http://localhost:3000/toys")
+   fetch("http://localhost:3000/toys")
 	  .then(function(response) {
 	    return response.json();
 	  })
